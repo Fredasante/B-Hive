a231e23dfb2d468bcad8cc05fc9c69cbe3b230fd
Alfred Asante Apenteng <alfredapenteng6996@gmail.com>
2024-05-13 23:50:14 +0000

    Added SignUp API routes

--- api/index.js
+++ api/index.js
@@ -1,13 +1,14 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
-    console.log("MongoDB connected"); // If connection is successful
+    console.log("MongoDB connected");
   })
   .catch((err) => {
     console.log(err);
@@ -15,6 +16,10 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000!!");
 });
+
+app.use("/api/auth", authRoutes);

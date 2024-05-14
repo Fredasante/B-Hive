2a7c36403da70ba034d2acefdacc623f9b07c227
Alfred Asante Apenteng <alfredapenteng6996@gmail.com>
2024-05-14 09:45:53 +0000

    Middleware & Error handing added

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error";
 
 export const signup = async (req, res) => {
   const { username, email, password } = req.body;
@@ -11,9 +12,7 @@ export const signup = async (req, res) => {
     email === "" ||
     password === ""
   ) {
-    return res.status(400).json({
-      message: "All fields are required",
-    });
+    next(errorHandler(400, "All fields are required"));
   }
 
   const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -28,8 +27,6 @@ export const signup = async (req, res) => {
     await newUser.save();
     res.json("Signup success");
   } catch (error) {
-    res.status(500).json({
-      message: error.message,
-    });
+    next(error);
   }
 };

89f263af0f8cd8ae8b34a6de56b822aa4953f20c
Alfred Asante Apenteng <alfredapenteng6996@gmail.com>
2024-05-14 14:19:24 +0000

    SignUp page UI done

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,21 +2,18 @@ import { Button, Navbar, TextInput } from "flowbite-react";
 import { Link, useLocation } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon } from "react-icons/fa";
+import logo from "../assets/logo.svg";
 
 const Header = () => {
   const path = useLocation().pathname;
 
   return (
     <Navbar className="border-b-2">
-      <Link
-        to="/"
-        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
-      >
-        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-          Fred's
-        </span>
-        Blog
+      <Link to="/" className="flex items-center gap-2">
+        <img src={logo} alt="logo" className="w-9 h-9" />
+        <p className="font-semibold text-xl">Blog Hive</p>
       </Link>
+
       <form>
         <TextInput
           type="text"
@@ -34,7 +31,7 @@ const Header = () => {
         </Button>
 
         <Link to="/sign-in">
-          <Button gradientDuoTone="purpleToBlue" outline>
+          <Button gradientDuoTone="cyanToBlue" outline>
             Sign In
           </Button>
         </Link>
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,65 @@
+import { Button, Label, TextInput } from "flowbite-react";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <section className="min-h-[60vh] mt-20">
+      <div className="flex flex-col md:flex-row md:items-center p-3 max-w-3xl mx-auto">
+        <div className="flex-1">
+          <Link to="/" className="font-bold text-4xl dark:text-white">
+            <span className="px-2 py-1 bg-[#0E7490] rounded-lg text-white">
+              Blog
+            </span>
+            Hive
+          </Link>
+          <p className="text-sm mt-5">
+            Sign up with your email and password or with Google
+          </p>
+        </div>
+
+        <div className="flex-1 border p-5 rounded-md">
+          <form className="flex flex-col gap-4">
+            <div>
+              <Label value="Your Username" />
+              <TextInput
+                type="text"
+                placeholder="Username"
+                id="username"
+                className="mt-1"
+              />
+            </div>
+            <div>
+              <Label value="Your Email" />
+              <TextInput
+                type="text"
+                placeholder="Email"
+                id="email"
+                className="mt-1"
+              />
+            </div>
+            <div>
+              <Label value="Your Password" />
+              <TextInput
+                type="text"
+                placeholder="Password"
+                id="password"
+                className="mt-1"
+              />
+            </div>
+
+            <Button type="submit">Sign Up</Button>
+          </form>
+          <div className="mt-5 flex gap-2 text-sm">
+            <span>Have an account?</span>
+            <Link to="/sign-in" className="text-teal-600">
+              Sign In
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default SignUp;

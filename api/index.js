e3f8fedfe406e5543e0999df2f1f89e5e056fd8a
Alfred Asante Apenteng <alfredapenteng6996@gmail.com>
2024-05-19 12:36:38 +0000

    Added create a post API Route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
+import postRoutes from "./routes/post.route.js";
 import cookieParser from "cookie-parser";
 
 dotenv.config();
@@ -27,6 +28,7 @@ app.use(cookieParser());
 // Define routes
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
+app.use("/api/post", postRoutes);
 
 // Error handling middleware
 app.use((err, req, res, next) => {

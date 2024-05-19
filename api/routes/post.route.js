119622f5187283e5d269d3d7a2782b5265699155
Alfred Asante Apenteng <alfredapenteng6996@gmail.com>
2024-05-19 23:55:24 +0000

    Added Update Post Functionality

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -4,12 +4,14 @@ import {
   create,
   deletepost,
   getposts,
+  updatepost,
 } from "../controllers/post.controller.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, create);
-router.get("/getPosts", getposts);
-router.delete("/deletePost/:postId/:userId", verifyToken, deletepost);
+router.get("/getposts", getposts);
+router.delete("/deletepost/:postId/:userId", verifyToken, deletepost);
+router.put("/updatepost/:postId/:userId", verifyToken, updatepost);
 
 export default router;

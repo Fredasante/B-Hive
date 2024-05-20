a3984a7f7b0e4bc0677ae24d735278e699123ca5
Alfred Asante Apenteng <alfredapenteng6996@gmail.com>
2024-05-20 23:01:00 +0000

    Comments to Posts Added

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   deleteUser,
   getUsers,
+  getUser,
   signout,
   test,
   updateUser,
@@ -15,5 +16,6 @@ router.put("/update/:userId", verifyToken, updateUser);
 router.delete("/delete/:userId", verifyToken, deleteUser);
 router.post("/signout", signout);
 router.get("/getusers", verifyToken, getUsers);
+router.get("/:userId", getUser);
 
 export default router;
--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,16 +1,17 @@
 import { Alert, Button, Textarea } from "flowbite-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import Comment from "./Comment";
 
 const CommentSection = ({ postId }) => {
   const [comment, setComment] = useState("");
-  const { currentUser } = useSelector((state) => state.user);
   const [errorMessage, setErrorMessage] = useState(null);
+  const [postComments, setPostComments] = useState([]);
+  const { currentUser } = useSelector((state) => state.user);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
     try {
       const res = await fetch("/api/comment/create", {
         method: "POST",
@@ -27,11 +28,27 @@ const CommentSection = ({ postId }) => {
       if (res.ok) {
         setComment("");
         setErrorMessage(null);
+        setPostComments([...postComments, data]);
       }
     } catch (error) {
       setErrorMessage(error.message);
     }
   };
+
+  useEffect(() => {
+    try {
+      const getPosts = async () => {
+        const res = await fetch(`/api/comment/getPostComments/${postId}`);
+        if (res.ok) {
+          const data = await res.json();
+          setPostComments(data);
+        }
+      };
+      getPosts();
+    } catch (error) {
+      console.log(error);
+    }
+  }, [postId]);
 
   return (
     <div className="max-w-2xl mx-auto w-full p-3">
@@ -85,6 +102,22 @@ const CommentSection = ({ postId }) => {
             </Alert>
           )}
         </form>
+      )}
+      {postComments.length === 0 ? (
+        <p className="text-sm my-5">No comments yet!</p>
+      ) : (
+        <>
+          <div className="text-sm my-5 flex items-center gap-1">
+            <p>Comments</p>
+            <div className="border border-gray-400 py-1 px-2 rounded-sm">
+              <p>{postComments.length}</p>
+            </div>
+          </div>
+
+          {postComments.map((comment) => (
+            <Comment key={comment._id} comment={comment} />
+          ))}
+        </>
       )}
     </div>
   );

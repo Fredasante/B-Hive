e4da4507d8875623e96b1425922a3dabbb5f4488
Alfred Asante Apenteng <alfredapenteng6996@gmail.com>
2024-05-21 00:00:49 +0000

    Added Like Functionality to Comment Component

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react";
 import moment from "moment";
+import { FaThumbsUp } from "react-icons/fa";
+import { useSelector } from "react-redux";
 
-const Comment = ({ comment }) => {
+const Comment = ({ comment, onLike }) => {
   const [user, setUser] = useState({});
+  const { currentUser } = useSelector((state) => state.user);
 
   useEffect(() => {
     const getUser = async () => {
@@ -18,6 +21,8 @@ const Comment = ({ comment }) => {
     };
     getUser();
   }, [comment]);
+
+  console.log(comment);
 
   return (
     <div className="flex p-4 border-b dark:border-gray-600 text-sm">
@@ -38,6 +43,29 @@ const Comment = ({ comment }) => {
           </span>
         </div>
         <p className="text-gray-500 pb-2">{comment.content}</p>
+        <div className="flex items-center pt-2 text-xs border-t dark:border-gray-700 max-w-fit gap-2">
+          <button
+            onClick={() => onLike(comment._id)}
+            type="button"
+            className={`text-gray-500 hover:text-teal-500 ${
+              currentUser &&
+              comment.likes.includes(currentUser._id) &&
+              "!text-teal-500"
+            }`}
+          >
+            <FaThumbsUp className=" text-sm" />
+          </button>
+          {comment.numberOfLikes > 0 &&
+            (comment.numberOfLikes === 1 ? (
+              <p className="text-xs text-gray-500">
+                {comment.numberOfLikes} like
+              </p>
+            ) : (
+              <p className="text-xs text-gray-500">
+                {comment.numberOfLikes} likes
+              </p>
+            ))}
+        </div>
       </div>
     </div>
   );
